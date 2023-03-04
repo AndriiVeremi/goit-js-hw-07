40b803d864dbb0a00b40e0cf3c3e93184143d0d4
Andrii Veremii <dashuk10@gmail.com>
2023-03-04 16:10:12 +0200

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.js-gallery');
+
+galleryContainer.innerHTML = galleryItems
+    .map(({ preview, original, description }) => {
+    return `
+            <li>
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            </li>
+            `;
+        })
+        .join('');
+
+const lightbox = new SimpleLightbox('.js-gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+
